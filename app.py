d41c177b4a3b2e71a5215d05ca4474433ec726c1
Code Freak <grgurinluka@gmail.com>
2023-10-20 21:16:29 +0200

    Full Version
    Added the full version with search and nice UI

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ functionDatabase = SQLAlchemy(app)
 
 create_post = False
 title = '~/Scriptopedia'
+func_search = ''
 
 
 class FunctionSite(functionDatabase.Model):
     id = functionDatabase.Column(functionDatabase.Integer, primary_key=True)
-    name = functionDatabase.Column(functionDatabase.String(12), nullable=False, default='bruh')
+    name = functionDatabase.Column(functionDatabase.String(30), nullable=False, default='bruh')
     description = functionDatabase.Column(functionDatabase.String(600), nullable=False)
-    author = functionDatabase.Column(functionDatabase.String(20), nullable=False, default='bruh')
-    language = functionDatabase.Column(functionDatabase.String(25), nullable=False, default='bruh')
+    language = functionDatabase.Column(functionDatabase.String(12), nullable=False, default='bruh')
     date_created = functionDatabase.Column(functionDatabase.DateTime, default=datetime.utcnow)
 
 
@@ -27,16 +27,18 @@ class FunctionSite(functionDatabase.Model):
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
+        global func_search
+        func_search = request.form['search']
         try:
             return redirect('/search')
         except:
-            return 'There creating your site!'
+            return 'Error: Could not search :('
 
     else:
         return render_template('index.html', create_post=create_post, title=title)
 
-@app.route('/create')
-def create():
+@app.route('/open')
+def open():
     global create_post, title
     create_post = True
     title = '~/Scriptopedia/make'
@@ -49,32 +51,45 @@ def close():
     title = '~/Scriptopedia'
     return redirect('/')
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    site_to_delete = FunctionSite.query.get_or_404(id)
-
-    try:
-        functionDatabase.session.delete(site_to_delete)
-        functionDatabase.session.commit()
-        return redirect('/')
-    except:
-        return 'There was a problem deleting that site'
-
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    site = FunctionSite.query.get_or_404(id)
-
+@app.route('/create', methods=['POST', 'GET'])
+def create():
     if request.method == 'POST':
-        site.content = request.form['content']
+        func_description = request.form['description']
+        func_name = request.form['name']
+        func_language = request.form['language']
+        new_func = FunctionSite(description=func_description, name=func_name, language=func_language)
 
         try:
+            functionDatabase.session.add(new_func)
             functionDatabase.session.commit()
             return redirect('/')
         except:
-            return 'There was an issue updating your task'
+            return 'Error: Could not create the function :('
 
     else:
-        return render_template('update.html', site=site)
+        return render_template('index.html')
+
+@app.route('/search')
+def search():
+    global func_search
+    functions = FunctionSite.query.order_by(FunctionSite.name).all()
+    return render_template('search.html', search=func_search, functions=functions)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    function_to_delete = FunctionSite.query.get_or_404(id)
+
+    try:
+        functionDatabase.session.delete(function_to_delete)
+        functionDatabase.session.commit()
+        return redirect('/')
+    except:
+        return 'Error: Could not delete the function :('
+
+@app.route('/function/<int:id>', methods=['GET', 'POST'])
+def function(id):
+    function = FunctionSite.query.get_or_404(id)
+    return render_template('function.html', function=function)
 
 if __name__ == "__main__":
     app.run(debug=True)
